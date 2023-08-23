9480df189239fcad61511e4f39ff4d6312427e3f
Joel Lasher <joel.lasher@sas.com>
2023-08-22 23:24:17 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::process;
+
+use rustgrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = rustgrep::run(config) {
+        eprintln!("Application Error: {e}");
+        process::exit(1);
+    }
+}
